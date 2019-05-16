1a939201ea6703e18b6b2b0a9aa5eaaeb79781fd
Danilo Moura <danilomourag@gmail.com>
2019-05-16 20:25:29 +1200

    Fix Controllers tests - Fixed problem with ILogger mock in PartsControllerTest - Created missing tests to add up on coverage - Fix identation on PartsController class

--- PartsTrader.ClientTools.Test/Controllers/PartsControllerTest.cs
+++ PartsTrader.ClientTools.Test/Controllers/PartsControllerTest.cs
@@ -1,32 +1,58 @@
-﻿//using System;
-//using System.Threading.Tasks;
-//using Microsoft.Extensions.Logging;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using Moq;
-//using PartsTrader.ClientTools.API.Controllers;
-//using PartsTrader.ClientTools.API.Service;
-//using PartsTrader.ClientTools.API.Validators;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PartsTrader.ClientTools.API.Controllers;
+using PartsTrader.ClientTools.API.Model.DTO;
+using PartsTrader.ClientTools.API.Service;
+using PartsTrader.ClientTools.API.Validators;
 
-//namespace PartsTrader.ClientTools.MSTest.Controllers
-//{
-//    [TestClass]
-//    public class PartsControllerTest
-//    {
-//        PartsController _controller = null;
-//        [TestMethod]
-//        public async Task GetPartsDetailsTest()
-//        {
-//            var _logger = new Mock<ILogger<PartsController>>();
-//            var _validator = new Mock<IPartsValidator>();
-//            var _service = new Mock<IPartsService>();
+namespace PartsTrader.ClientTools.MSTest.Controllers
+{
+    [TestClass]
+    public class PartsControllerTest
+    {
+        [TestMethod]
+        public async Task GetPartsDetailsTest()
+        {
+            var partNumber = "1111-aaaa";
+            var _logger = new Mock<ILogger<PartsController>>();
+            var _validator = new Mock<IPartsValidator>();
+            var _service = new Mock<IPartsService>();
 
-//            _controller = new PartsController(_logger, _validator.Object, _service.Object);
+            _validator.Setup(x => x.IsPartNumberValid(It.IsAny<string>())).Returns(true);
+            _service.SetReturnsDefault<PartDetailsDTO>(new PartDetailsDTO(){ PartNumber = partNumber });
 
-//            _validator.Setup(x => x.IsPartNumberValid(It.IsAny<string>())).Returns(true);
+            var _controller = new PartsController(_logger.Object, _validator.Object, _service.Object);
 
-//            var result = await _controller.GetCompatibleParts("1111-aaaa");
+            var result = await _controller.GetPartDetails(partNumber);
 
-//            return Task.Run(() => );
-//        }
-//    }
-//}
+            Assert.IsInstanceOfType(result, typeof(ActionResult<PartDetailsDTO>));
+            Assert.IsInstanceOfType(result.Result, typeof(OkObjectResult));
+            Assert.IsNotNull(_service.Invocations);
+        }
+
+        [TestMethod]
+        public async Task GetCompatibleParts()
+        {
+            var partNumber = "1111-aaaa";
+            var _logger = new Mock<ILogger<PartsController>>();
+            var _validator = new Mock<IPartsValidator>();
+            var _service = new Mock<IPartsService>();
+
+            _validator.Setup(x => x.IsPartNumberValid(It.IsAny<string>())).Returns(true);
+            _service.SetReturnsDefault<List<PartSummaryDTO>>(new List<PartSummaryDTO>(){ new PartSummaryDTO() { PartNo = partNumber } });
+
+            var _controller = new PartsController(_logger.Object, _validator.Object, _service.Object);
+
+            var result = await _controller.GetCompatibleParts(partNumber);
+
+            Assert.IsInstanceOfType(result, typeof(ActionResult<IEnumerable<PartSummaryDTO>>));
+            Assert.IsInstanceOfType(result.Result, typeof(OkObjectResult));
+            Assert.IsNotNull(_service.Invocations);
+        }
+    }
+}
